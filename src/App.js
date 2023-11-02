45eb653cee3e0ed7c3881ec575d1a04a235624a9
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-02 17:06:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="app">
-      <h1>hi there</h1>
+    <div className="app">      
+      <Header />
+      {/* Home */}
     </div>
   );
 }
