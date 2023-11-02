be9bdbefa76911bb4bcca3ef889891fb90ace34f
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-02 21:40:51 +0200

    Refactor React Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className="app">      
-        <Header />
-        <Home />      
+      <div className="app">
+        <Routes>
+          <Route path="/" element={((<Header />), (<Home />))} />          
+        </Routes>
       </div>
     </Router>
   );
