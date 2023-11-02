0a330926a278a14d3b585983143f2ba397f2e2ae
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-02 21:23:08 +0200

    Add react-router-dom and wrap App component with Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">      
-      <Header />
-      <Home />      
-    </div>
+    <Router>
+      <div className="app">      
+        <Header />
+        <Home />      
+      </div>
+    </Router>
   );
 }
 
