1c7c97e70aaa951a28b32f34eed7e0f5a108d29c
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-05 20:28:48 +0200

    Add alt attribute to Amazon logo and format register button.

--- src/Login.js
+++ src/Login.js
@@ -9,6 +9,7 @@ function Login() {
         <img
           className="login__logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
         />
       </Link>
       <div className="login__container">
@@ -25,7 +26,9 @@ function Login() {
           see our Privacy Notice, our Cookies Notice and our Interest-Based Ads
           Notice.
         </p>
-        <button className="login__registerButton">Create your Amazon Account</button>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
