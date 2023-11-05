ef2a31e6f6ff91254f51bd74c65d0032ee6c854f
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-05 18:36:17 +0200

    Add user email and checkout product component to Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,29 +1,39 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img
           className="checkout__ad"
-          src="https://m.media-amazon.com/images/I/71cvRGs+pPL._SX3000_.jpg"
+          src="https://m.media-amazon.com/images/I/71Ie3JXGfVL._SX3000_.jpg"
           alt=""
         />
+
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
+
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
+
       <div className="checkout__right">
         <Subtotal />
-        <h2>The subtotal will go here</h2>
       </div>
     </div>
   );
