cd6402b01714ef5d3ca3b177b7026a3d110b602c
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-07 01:03:50 +0200

    Update Firebase version and add user authentication functionality

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
       <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,37 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const signIn = (e) => {
-    e.preventDefault();
-    // firebase login
-  };
+const signIn = (e) => {
+  e.preventDefault();
 
-  const register = (e) => {
-    e.preventDefault();
-    // firebase register
-  };
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((auth) => {
+      navigate("/");
+    })
+    .catch((error) => alert(error.message));
+};
+
+const register = (e) => {
+  e.preventDefault();
+
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((auth) => {
+      // it successfully created a new user with email and password
+      if (auth) {
+        navigate("/");
+      }
+    })
+    .catch((error) => alert(error.message));
+};
 
   return (
     <div className="login">
