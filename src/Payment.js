a4f81312bbf101a4201d7e67cc561fdbfc360455
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-08 13:06:18 +0200

    Add axios for payment processing

--- src/Payment.js
+++ src/Payment.js
@@ -1,23 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
+import axios from "./axios";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
 
   const stripe = useStripe();
   const elements = useElements();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
+
+  useEffect(() => {
+    // generate the special stripe secret which allows us to charge a customer
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        // Stripe expects the total in a currencies subunits
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+    getClientSecret();
+  }, [basket]);
+
+  const handleSubmit = async (e) => {
     // do all the fancy stripe stuff...
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        // paymentIntent = payment confirmation
+
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        dispatch({
+          type: "EMPTY_BASKET",
+        });
+
+        navigate.replace("/orders");
+      });
   };
 
   const handleChange = (e) => {
     // Listen for changes in the CardElement
-  };   //6:00:32   Part 8: Payment Processing
+    // and display any errors as the customer types their card details
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -62,6 +111,21 @@ function Payment() {
             {/* Stripe magic will go */}
             <form onSubmit={handleSubmit}>
               <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {/* Errors */}
+              {error && <div>{error}</div>}
             </form>
           </div>
         </div>
--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,9 @@
-import axios from "./axios";
+import axios from "axios";
 
 // URL for deployed cloudfunction
 const instance = axios.create({
-  baseURL: "https://us-central1-challenge-4b2b2.cloudfunctions.net/api",
-  // "http://localhost:5001/challenge-4b2b2/us-central1/api",
-  // "http://localhost:9500/stripe-checkout-test-5fcb8/us-central1/api",
-  // "https://us-central1-stripe-checkout-test-5fcb8.cloudfunctions.net/api",
+  baseURL: 
+    "..." // The API (cloud function) URL
 });
 
 export default instance;
